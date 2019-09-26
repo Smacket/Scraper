df9fae07911de6b19d3e9e0677d6cce71e588a1d
MCAR43 <MCAR43@mst.edu>
2019-09-26 15:03:01 -0500

    Added a bit more functionality/better scraping

--- Scraper.py
+++ Scraper.py
@@ -1,39 +1,56 @@
 #!/usr/bin/env python3
 from selenium import webdriver
 import time
-#TODO: Use beautiful soup to extract the correct youtube URLs from smashvods
-#      Navigate to an arbitrary time in the video e.g.:
-#       - https://www.youtube.com/watch?v=BD5eP6lk32A
-#       - becomes --> https://www.youtube.com/watch?v=BD5eP6lk32A&t=200s
-#      Full Screen the video, and take screenshot with filename corresponding to names and characters
-#      Pulled from Beautiful Soup
-
+URL = "https://sf.vods.co/ultimate/character"
 CHARACTERS = [
     "Banjo",
     "Bayonetta",
     "Bowser",
-    "Mario"
-        ]
+    "Bowser Jr."
+]
+
+class VideoLink:
+    def __init__(self, charA, charB):
+        self.charA = charA
+        self.charB = charB
+        self.links = []
+    
+    def addLink(self, link):
+        self.links.append(link)
+
+    def getLinks(self):
+        return self.links
+
+    def display(self):
+        print("%s vs %s:" % (self.charA, self.charB))
+        for link in self.links:
+            print(link, sep=", ", end="")
+
+#Fill this out
+def getScrotFromVideo(videoLinks):
+    pass
 
 
+def getVideoLinks(browser, charA, charB):
+    temp = VideoLink(charA, charB)
+    browser.get(URL + charA + "/character2/" + charB)
+    vodTableList = browser.find_element_by_xpath('/html/body/div[2]/div/div[3]/div/div[1]/div/section/div/div/div/div')
+    for tR in vodTableList.find_elements_by_css_selector('tr'):
+        linkElement = tR.find_elements_by_css_selector('td')[2]
+        smashVodsLink = linkElement.find_element_by_css_selector('a').get_attribute('href')
+        print("%s vs %s --> %s" % (charA, charB, smashVodsLink))
+        temp.addLink(smashVodsLink)
+
+    return temp
 
 def main():
     browser = webdriver.Chrome()
-    URL = "https://sf.vods.co/ultimate/character"
-    
+    relevantLinks = []
     for charA in CHARACTERS:
         for charB in CHARACTERS:
-            if charA == charB:
-                break
+            relevantLinks = getVideoLinks(browser, charA, charB)
+            relevantLinks.display()
 
-            browser.get(URL + charA + "/character2/" + charB)
-            time.sleep(3)
-            vodTableList = browser.find_element_by_xpath('/html/body/div[2]/div/div[3]/div/div[1]/div/section/div/div/div/div')
-            for tR in vodTableList.find_elements_by_css_selector('tr'):
-                linkElement = tR.find_elements_by_css_selector('td')[2]
-                smashVodsLink = linkElement.find_element_by_css_selector('a').get_attribute('href')
-                print(smashVodsLink) 
-    #browser.save_screenshot('image.png')
     browser.quit()
 
 if __name__ == "__main__":
